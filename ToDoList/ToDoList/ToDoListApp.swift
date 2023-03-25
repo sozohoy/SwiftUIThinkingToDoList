d410013be68749d3fd9a175222c01f9db8b1788d
sozohoy <sozohoy@gmail.com>
2023-03-25 15:38:57 +0900

    project initialize

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 한지석 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
