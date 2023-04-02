463c31a66e1c92576244504607278401c8c2fc1b
sozohoy <sozohoy@gmail.com>
2023-04-02 18:06:24 +0900

    5. User Experience and Animations

--- ToDoList/ToDoList/Views/ListView.swift
+++ ToDoList/ToDoList/Views/ListView.swift
@@ -13,18 +13,25 @@ struct ListView: View {
     
     var body: some View {
         
-        // List 형식으로 데이터 출력. TableView와 유사
-        List {
-            ForEach(listViewModel.items) { item in
-                ListRowView(item: item)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            listViewModel.updateItem(item: item)
-                        }
+        ZStack {
+            // List 형식으로 데이터 출력. TableView와 유사
+            if listViewModel.items.isEmpty {
+                NoItemsView()
+                    .transition(AnyTransition.opacity.animation(.easeIn))
+            } else {
+                List {
+                    ForEach(listViewModel.items) { item in
+                        ListRowView(item: item)
+                            .onTapGesture {
+                                withAnimation(.linear) {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
                     }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
             }
-            .onDelete(perform: listViewModel.deleteItem)
-            .onMove(perform: listViewModel.moveItem)
         }
         .listStyle(PlainListStyle())
         .navigationTitle("To Do List")
---
+++ ToDoList/ToDoList/Views/NoItemsView.swift
@@ -0,0 +1,74 @@
+//
+//  NoItemsView.swift
+//  ToDoList
+//
+//  Created by 한지석 on 2023/04/02.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items !")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("계획을 추가하세요! add 버튼을 누르시면, 원하는 To do list를 만들 수 있어요!")
+                    .padding(.bottom, 20)
+                NavigationLink {
+                    AddView()
+                } label: {
+                    Text("계획 추가하기! 📜")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? secondaryAccentColor : Color.accentColor)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                        radius: animate ? 30 : 10,
+                        x: 0,
+                        y: animate ? 50 : 30)
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
+    }
+    
+    
+}
+
+struct NoItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            NoItemsView()
+                .navigationTitle("Title")
+        }
+        
+    }
+}
